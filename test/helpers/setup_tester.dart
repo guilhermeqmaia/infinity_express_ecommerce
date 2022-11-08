1eb3e69e6b7306a0fc89e1096f680f2eefbc602d
guilhermemaiawarren <guilherme.maia@warren.com.br>
2022-11-08 10:43:18 -0300

    first commit test branch

---
+++ test/helpers/setup_tester.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SetupTester extends StatelessWidget {
+  const SetupTester({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(''),
+      ),
+      body: Container(),
+    );
+  }
+}
